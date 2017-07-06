7449a8db0d24a659bdf6b8f04b36e6fc6c7e0ff1
Thierry Carrez <thierry@openstack.org>
2017-07-06 11:44:26 +0200

    Add instructions related to the PTGbot IRC bot
    Add Puppet instructions to run the PTGbot IRC bot. This
    is based on the puppet-statusbot module, and tweaked to
    conform to PTGbot configuration file format.
    
    Configurable elements include bot nickname, password, and
    channel. Other elements (like ethercalc pointers) will
    require to be refreshed in this module for each PTG event.
    
    The next step would be to set up a web server to serve
    static content from /var/lib/ptgbot/www.
    
    Change-Id: Ia3c5a44c33dab1914c9908280bec1323e35431f2

---
+++ Gemfile
@@ -0,0 +1,11 @@
+source 'https://rubygems.org'
+
+group :development, :test, :system_tests do
+  gem 'puppet-openstack_infra_spec_helper',
+      :git     => 'https://git.openstack.org/openstack-infra/puppet-openstack_infra_spec_helper',
+      :require => false
+  # Not all modules can do this, so we add it here to the ones that can
+  gem 'puppet-lint-empty_string-check'
+end
+
+# vim:ft=ruby
---
+++ manifests/init.pp
@@ -0,0 +1,137 @@
+# == Class: ptgbot
+#
+class ptgbot(
+  $nick,
+  $password,
+  $channel,
+) {
+
+  user { 'ptgbot':
+    ensure     => present,
+    home       => '/home/ptgbot',
+    shell      => '/bin/bash',
+    gid        => 'ptgbot',
+    managehome => true,
+    require    => Group['ptgbot'],
+  }
+
+  group { 'ptgbot':
+    ensure => present,
+  }
+
+  vcsrepo { '/opt/ptgbot':
+    ensure   => latest,
+    provider => git,
+    revision => 'master',
+    source   => 'https://git.openstack.org/openstack/ptgbot',
+  }
+
+  exec { 'install_ptgbot' :
+    command     => 'pip3 install /opt/ptgbot',
+    path        => '/usr/local/bin:/usr/bin:/bin/',
+    refreshonly => true,
+    subscribe   => Vcsrepo['/opt/ptgbot'],
+  }
+
+  file { '/etc/init.d/ptgbot':
+    ensure  => present,
+    group   => 'root',
+    mode    => '0555',
+    owner   => 'root',
+    require => Vcsrepo['/opt/ptgbot'],
+    source  => 'puppet:///modules/ptgbot/ptgbot.init',
+  }
+
+  service { 'ptgbot':
+    enable     => true,
+    hasrestart => true,
+    require    => File['/etc/init.d/ptgbot'],
+    subscribe  => [
+      Vcsrepo['/opt/ptgbot'],
+      File['/etc/ptgbot/ptgbot_config.json'],
+    ],
+  }
+
+  file { '/etc/ptgbot':
+    ensure => directory,
+  }
+
+  file { '/var/log/ptgbot':
+    ensure  => directory,
+    owner   => 'ptgbot',
+    group   => 'ptgbot',
+    mode    => '0775',
+    require => User['ptgbot'],
+  }
+
+  file { '/var/run/ptgbot':
+    ensure  => directory,
+    owner   => 'ptgbot',
+    group   => 'ptgbot',
+    mode    => '0775',
+    require => User['ptgbot'],
+  }
+
+  file { '/var/lib/ptgbot':
+    ensure  => directory,
+    owner   => 'ptgbot',
+    group   => 'ptgbot',
+    mode    => '0775',
+    require => User['ptgbot'],
+  }
+
+  file { '/var/lib/ptgbot/www':
+    ensure  => directory,
+    owner   => 'ptgbot',
+    group   => 'ptgbot',
+    mode    => '0775',
+    require => [File['/var/lib/ptgbot'],
+                User['ptgbot']]
+  }
+
+  file { '/var/lib/ptgbot/www/ptg.html':
+    ensure    => present,
+    group     => 'ptgbot',
+    mode      => '0440',
+    owner     => 'root',
+    replace   => true,
+    require   => [File['/var/lib/ptgbot/www'],
+                  User['ptgbot']],
+    source    => '/opt/ptgbot/html/ptg.html',
+    subscribe => Vcsrepo['/opt/ptgbot'],
+  }
+
+  file { '/var/lib/ptgbot/www/ptg.js':
+    ensure    => present,
+    group     => 'ptgbot',
+    mode      => '0440',
+    owner     => 'root',
+    replace   => true,
+    require   => [File['/var/lib/ptgbot/www'],
+                  User['ptgbot']],
+    source    => '/opt/ptgbot/html/ptg.js',
+    subscribe => Vcsrepo['/opt/ptgbot'],
+  }
+
+  file { '/etc/ptgbot/logging.config':
+    ensure  => present,
+    group   => 'ptgbot',
+    mode    => '0440',
+    owner   => 'root',
+    replace => true,
+    require => User['ptgbot'],
+    source  => 'puppet:///modules/ptgbot/logging.config',
+  }
+
+  file { '/etc/ptgbot/ptgbot_config.json':
+    ensure  => present,
+    content => template('ptgbot/ptgbot_config.json.erb'),
+    group   => 'ptgbot',
+    mode    => '0440',
+    owner   => 'root',
+    replace => true,
+    require => User['ptgbot'],
+  }
+}
+
+# vim:sw=2:ts=2:expandtab:textwidth=79
